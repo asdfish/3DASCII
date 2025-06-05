261b808443abf56f75797ad708b570da0207da50
xelphyr <xelphyrnull@gmail.com>
2025-06-05 11:52:30 +0530

    start

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t; cin>>t;
+    while (t--)
+    {
+        int n; cin>>n;
+        if (n>5)
+        {
+            cout<<"Greater than 5";
+        }
+        else
+        {
+            cout<<"Less than 5";
+        }
+    }
+}
+
