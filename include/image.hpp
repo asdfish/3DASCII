263050b0c0164eec162f80c3cb0dbc0ca5985066
xelphyr <xelphyrnull@gmail.com>
2025-06-05 20:07:32 +0530

    Modified directories and started work on bitmap file handling

---
+++ include/image.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+#pragma once
+
+struct float3
+{
+    union
+    {
+        struct {float x; float y; float z;};
+        struct {float r; float g; float b;};
+    };
+};
+
+class Image
+{
+    public :
+        Image(int width, int height);
+        ~Image();
+
+        float3 GetColor(int x, int y) const;
+        void SetColor(const float3& color, int x, int y);
+
+        void Export(const char* path);
+
+    private:
+        int m_width;
+        int m_height;
+        std::vector<float3> m_colors;
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -22,5 +22,5 @@ int main() {
     std::cout << "Sum of values: " << result << std::endl;
 
     return 0;
-}
+} 
 
