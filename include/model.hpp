9aa3709ec539edeef9490c6957bbeeadbf01e610
xelphyr <xelphyrnull@gmail.com>
2025-06-06 15:42:48 +0530

    Made a Transform class to hold pos, rot, parent details

--- include/model.hpp
+++ include/model.hpp
@@ -3,14 +3,28 @@
 
 #pragma once
 
+//contains position, rotation details of a sceneobject
 class Transform
 {
-    
+    public:
+        Transform(Transform* parent = nullptr, float3 rot = float3(), float3 pos = float3());
+        ~Transform();
+
+        const float3& GetPos();
+        const float3& GetRot();
+        const Transform* GetParent();
+
+        void SetPos(const float3& newPos);
+        void SetRot(const float3& newRot);
+        void SetParent(const Transform* parent);
+
+    private:
+        float3 m_pos;
+        float3 m_rot;
+        Transform* m_parent;
 };
 
-
-
-//Using for anything with a position, like vertices of a model, etc.
+//Contains mesh details of individual meshes
 class Model
 {
     public:
