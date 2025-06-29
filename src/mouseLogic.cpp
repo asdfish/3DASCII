814d163d5d968d52087c33f03f3ecb0a8f38e06d
xelphyr <168811362+xelphyr@users.noreply.github.com>
2025-06-29 17:52:43 +0530

    Update mouseLogic.cpp

--- src/mouseLogic.cpp
+++ src/mouseLogic.cpp
@@ -1,6 +1,6 @@
 #include "ui/mouseLogic.hpp"
 #include <imgui.h>
-#include <imgui_impl_SDL3.h>
+#include <imgui_impl_sdl3.h>
 #include <imgui_impl_sdlrenderer3.h>
 
 inline float FastSmoothFalloff(float x, float minBound, float maxBound);
